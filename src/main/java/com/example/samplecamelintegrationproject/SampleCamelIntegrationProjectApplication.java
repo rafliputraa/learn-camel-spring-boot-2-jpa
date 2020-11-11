51f15ef41699a6d046f2a2512684bb68ef2f3c1d
rafliputraa <rafli.putra712@gmail.com>
2020-11-11 13:42:52 +0700

    first-commit

---
+++ src/main/java/com/example/samplecamelintegrationproject/SampleCamelIntegrationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplecamelintegrationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleCamelIntegrationProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleCamelIntegrationProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/samplecamelintegrationproject/SampleCamelIntegrationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplecamelintegrationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleCamelIntegrationProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
